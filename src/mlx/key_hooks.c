2e7b331c1e4c586afdb52e8a464728cf4a2ff8b4
Christian <christian.rasche@bertelsmann.de>
2024-09-23 16:23:14 +0200

    Added straving and arrow key support

--- src/mlx/key_hooks.c
+++ src/mlx/key_hooks.c
@@ -29,6 +29,7 @@ int	is_wall(t_data *data, float x, float y)
 
 // WSL: W 119, A 97, S 115, D 100
 // MAC: W 13, A 0, S 1, D 2, ESC 53
+// KEY:124 KEY:125 KEY:126 KEY:123
 int	key_hook_ws(int keycode, void *param)
 {
 	t_data	*data;
@@ -36,7 +37,7 @@ int	key_hook_ws(int keycode, void *param)
 	float	new_y;
 
 	data = (t_data *)param;
-	if (keycode == 119 || keycode == 13)
+	if (keycode == 119 || keycode == 13 || keycode == 126)
 	{
 		printf("KEY:%d ", keycode);
 		new_x = data->player.pos[X] + (data->player.direct[X] * STEP_SIZE);
@@ -47,11 +48,41 @@ int	key_hook_ws(int keycode, void *param)
 			data->player.pos[Y] = new_y;
 		}
 	}
-	else if (keycode == 115 || keycode == 1)
+	else if (keycode == 115 || keycode == 1 || keycode == 125)
 	{
 		printf("KEY:%d ", keycode);
 		new_x = data->player.pos[X] - (data->player.direct[X] * STEP_SIZE);
 		new_y = data->player.pos[Y] - (data->player.direct[Y] * STEP_SIZE);
+		if (!is_wall(data, new_x, new_y))
+		{
+			data->player.pos[X] = new_x;
+			data->player.pos[Y] = new_y;
+		}
+	}
+	if (keycode == 97 || keycode == 0)
+	{
+		printf("KEY:%d ", keycode);
+		float temp_x = data->player.direct[X] * cos(-90 * M_PI / 180) - data->player.direct[Y] * sin(-90 * M_PI / 180);
+		float temp_y = data->player.direct[X] * sin(-90 * M_PI / 180) + data->player.direct[Y] * cos(-90 * M_PI / 180);
+		temp_x /= sqrt(temp_x * temp_x + temp_y * temp_y);
+		temp_y /= sqrt(temp_x * temp_x + temp_y * temp_y);
+		new_x = data->player.pos[X] + (temp_x * STEP_SIZE);
+		new_y = data->player.pos[Y] + (temp_y * STEP_SIZE);
+		if (!is_wall(data, new_x, new_y))
+		{
+			data->player.pos[X] = new_x;
+			data->player.pos[Y] = new_y;
+		}
+	}
+	else if (keycode == 100 || keycode == 2)
+	{
+		printf("KEY:%d ", keycode);
+		float temp_x = data->player.direct[X] * cos(90 * M_PI / 180) - data->player.direct[Y] * sin(90 * M_PI / 180);
+		float temp_y = data->player.direct[X] * sin(90 * M_PI / 180) + data->player.direct[Y] * cos(90 * M_PI / 180);
+		temp_x /= sqrt(temp_x * temp_x + temp_y * temp_y);
+		temp_y /= sqrt(temp_x * temp_x + temp_y * temp_y);
+		new_x = data->player.pos[X] + (temp_x * STEP_SIZE);
+		new_y = data->player.pos[Y] + (temp_y * STEP_SIZE);
 		if (!is_wall(data, new_x, new_y))
 		{
 			data->player.pos[X] = new_x;
@@ -69,7 +100,7 @@ int	key_hook_ad(int keycode, void *param)
 	t_data	*data;
 
 	data = (t_data *)param;
-	if (keycode == 97 || keycode == 0)
+	if (keycode == 97 || keycode == 10 || keycode == 123)
 	{
 		printf("KEY:%d ", keycode);
 		data->player.direct[X] = data->player.direct[X] * cos(-0.1) - data->player.direct[Y] * sin(-0.1);
@@ -77,7 +108,7 @@ int	key_hook_ad(int keycode, void *param)
 		data->player.direct[X] /= sqrt(data->player.direct[X] * data->player.direct[X] + data->player.direct[Y] * data->player.direct[Y]);
 		data->player.direct[Y] /= sqrt(data->player.direct[X] * data->player.direct[X] + data->player.direct[Y] * data->player.direct[Y]);
 	}
-	else if (keycode == 100 || keycode == 2)
+	else if (keycode == 100 || keycode == 22 || keycode == 124)
 	{
 		printf("KEY:%d ", keycode);
 		data->player.direct[X] = data->player.direct[X] * cos(0.1) - data->player.direct[Y] * sin(0.1);
@@ -119,6 +150,7 @@ int	key_hook_esc_mac(int keycode, void *param)
 
 int	key_hook(int keycode, void *param)
 {
+	// printf("KEY:%d ", keycode);
 	key_hook_ws(keycode, param);
 	key_hook_ad(keycode, param);
 	key_hook_esc_mac(keycode, param);
